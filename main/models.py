1269f4c59942efe0a21eefafba735cdfeeb984a2
Andrey <yuryev@yandex.ru>
2024-02-23 15:49:57 +0300

    создание viewset и generics

--- main/models.py
+++ main/models.py
@@ -1,19 +1,5 @@
 from django.db import models
-
 from main.utils import NULLABLE
-
-
-class Lesson(models.Model):
-    title = models.CharField(max_length=100, verbose_name='название урока')
-    description = models.TextField(**NULLABLE, verbose_name='описание урока')
-    preview = models.ImageField(upload_to='lessons/', verbose_name='превью', **NULLABLE)
-
-    def __str__(self):
-        return f'{self.title}'
-
-    class Meta:
-        verbose_name = 'урок'
-        verbose_name_plural = 'уроки'
 
 
 class Course(models.Model):
@@ -21,7 +7,6 @@ class Course(models.Model):
     description = models.TextField(**NULLABLE, verbose_name='описание курса')
     preview = models.ImageField(upload_to='lessons/', verbose_name='превью', **NULLABLE)
     video_link = models.CharField(max_length=255, verbose_name='ссылка на видео', **NULLABLE)
-    lessons = models.ManyToManyField(Lesson, verbose_name='уроки')
 
     def __str__(self):
         return f'{self.title}'
@@ -29,3 +14,17 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'курс'
         verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название урока')
+    description = models.TextField(**NULLABLE, verbose_name='описание урока')
+    preview = models.ImageField(upload_to='lessons/', verbose_name='превью', **NULLABLE)
+    #course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
--- main/views.py
+++ main/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics
+from main.serializer import LessonSerializer, CourseSerializer
+from main.models import Lesson, Course
 
-# Create your views here.
+
+class LessonViewSet(viewsets.ModelViewSet):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class CourseCreateAPIView(generics.CreateAPIView):
+    serializer_class = CourseSerializer
+
+
+class CourseDestroyAPIView(generics.DestroyAPIView):
+    queryset = Course.objects.all()
+
+
+class CourseRetriveAPIView(generics.RetrieveAPIView):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class CourseListAPIView(generics.ListAPIView):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class CourseUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
