e1f96e1a46da26b56e31d99b1514e02817bd44cf
Andrey <yuryev@yandex.ru>
2024-02-26 22:56:55 +0300

    serializer для модели курсов. изменение

--- lms/models.py
+++ lms/models.py
@@ -19,7 +19,8 @@ class Lesson(models.Model):
     title = models.CharField(max_length=100, verbose_name='название урока')
     description = models.TextField(**NULLABLE, verbose_name='описание урока')
     preview = models.ImageField(upload_to='lessons/', verbose_name='превью', **NULLABLE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+    # course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс', related_name='lesson')
     video_link = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
 
     def __str__(self):
