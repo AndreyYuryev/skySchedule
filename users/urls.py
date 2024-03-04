b5304692c4842e22eb982d0a609de81e1b53a5bb
Andrey <yuryev@yandex.ru>
2024-03-04 23:15:56 +0300

    добавление permissions

--- users/urls.py
+++ users/urls.py
@@ -8,11 +8,10 @@ app_name = UsersConfig.name
 
 router = SimpleRouter()
 router.register(r'users', UserViewSet, basename='users')
-print(router.urls)
+
 urlpatterns = [
                   path('payment/', PaymentListAPIView.as_view(), name='payment_list'),
                   path('user/<int:pk>/payment', UserPaymentRetrieveAPIView.as_view(), name='user_payment'),
                   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
                   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-                  # path('api', include(router.urls)),
               ] + router.urls
--- users/views.py
+++ users/views.py
@@ -5,13 +5,21 @@ from users.models import User, Payment
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from lms.permissions import IsOwner
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            permission_classes = [IsAuthenticated, ]
+        else:
+            permission_classes = [IsAuthenticated, IsOwner]
+        return [permission() for permission in permission_classes]
 
     def create(self, request, *args, **kwargs):
         serializer = UserSerializer(data=request.data)
